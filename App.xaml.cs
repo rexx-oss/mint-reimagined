419295b18b673a28c72dc17c9a1b66c99222f263
rexx-oss <crazyrexxar@gmail.com>
2026-09-20 05:19:06 +0800

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading;
 using System.Windows;
 
@@ -6,20 +7,33 @@ namespace Mint
     public partial class App : Application
     {
         private static Mutex? _mutex;
-        private const string MutexId = "MINT_LAUNCHER_SINGLE_INSTANCE";
+        private const string MutexName = "MINT_LAUNCHER_SINGLE_INSTANCE_MUTEX";
 
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            _mutex = new Mutex(true, MutexId, out bool isNew);
-            if (!isNew)
+            // Catch all unhandled startup crashes to prevent silent exits
+            AppDomain.CurrentDomain.UnhandledException += (s, args) =>
             {
-                MessageBox.Show("Mint is already active in your taskbar.", "Mint", MessageBoxButton.OK, MessageBoxImage.Information);
+                MessageBox.Show($"Mint startup error:\n{args.ExceptionObject}", "Mint Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            };
+
+            DispatcherUnhandledException += (s, args) =>
+            {
+                MessageBox.Show($"Mint UI error:\n{args.Exception.Message}", "Mint Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                args.Handled = true;
+            };
+
+            _mutex = new Mutex(true, MutexName, out bool isNewInstance);
+            if (!isNewInstance)
+            {
+                MessageBox.Show("Mint is already running. Check your taskbar near the clock.", "Mint", MessageBoxButton.OK, MessageBoxImage.Information);
                 Shutdown();
                 return;
             }
 
             var mainWindow = new MainWindow();
-            mainWindow.Hide();
+            // Opens centered on screen immediately on launch
+            mainWindow.Show();
         }
     }
 }
