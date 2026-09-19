8e5fb5379b122fa1626117d8c59085945e566b67
rexx-oss <crazyrexxar@gmail.com>
2026-09-20 05:07:15 +0800

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,14 +6,14 @@ namespace Mint
     public partial class App : Application
     {
         private static Mutex? _mutex;
-        private const string MutexId = "MINT_LAUNCHER_SINGLE_INSTANCE_MUTEX";
+        private const string MutexId = "MINT_LAUNCHER_SINGLE_INSTANCE";
 
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            _mutex = new Mutex(true, MutexId, out bool isNewInstance);
-            if (!isNewInstance)
+            _mutex = new Mutex(true, MutexId, out bool isNew);
+            if (!isNew)
             {
-                MessageBox.Show("Mint is already running in your system tray.", "Mint", MessageBoxButton.OK, MessageBoxImage.Information);
+                MessageBox.Show("Mint is already active in your taskbar.", "Mint", MessageBoxButton.OK, MessageBoxImage.Information);
                 Shutdown();
                 return;
             }
