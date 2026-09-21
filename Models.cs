18799da42bb51c3f1bf12f1b43f7795f8214d852
rexx-oss <crazyrexxar@gmail.com>
2026-09-21 10:46:47 +0800

    Update Models.cs

--- Models.cs
+++ Models.cs
@@ -1,29 +1,27 @@
 using System;
 using System.Collections.Generic;
 
-namespace Mint
+namespace Mint;
+
+public enum AppTheme
 {
-    public enum AppTheme
-    {
-        System,
-        Dark,
-        Light
-    }
+    System,
+    Dark,
+    Light
+}
 
-    public class AppItem
-    {
-        public Guid Id { get; set; } = Guid.NewGuid();
-        public string AppTitle { get; set; } = string.Empty;
-        public string AppLink { get; set; } = string.Empty;
-        public string AppGroup { get; set; } = string.Empty;
-        public string AppParams { get; set; } = string.Empty;
-        public string CustomIconPath { get; set; } = string.Empty;
-    }
+public class AppItem
+{
+    public string AppTitle { get; set; } = string.Empty;
+    public string AppLink { get; set; } = string.Empty;
+    public string AppGroup { get; set; } = string.Empty;
+    public string AppParams { get; set; } = string.Empty;
+    public string CustomIconPath { get; set; } = string.Empty;
+}
 
-    public class AppSettings
-    {
-        public AppTheme Theme { get; set; } = AppTheme.System;
-        public bool StartWithWindows { get; set; } = false;
-        public List<AppItem> Apps { get; set; } = new();
-    }
+public class AppSettings
+{
+    public AppTheme Theme { get; set; } = AppTheme.System;
+    public bool StartWithWindows { get; set; } = false;
+    public List<AppItem> Apps { get; set; } = new();
 }
